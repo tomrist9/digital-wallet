f45f493da344418e74e2d0a8df24c0ff1a1a6506
tomrist9 <tomristt9@gmail.com>
2026-03-03 15:46:31 +0400

    feat: add config class and logback.xml

--- ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/config/AIGeneratedTweetToKafkaServiceConfigData.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/config/AIGeneratedTweetToKafkaServiceConfigData.java
@@ -1,9 +1,14 @@
 package com.ai.generated.tweet.to.kafka.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "ai-generated-tweet-to-kafka-service")
 public class AIGeneratedTweetToKafkaServiceConfigData {
+    private List<String> streamingDataKeywords;
 }
