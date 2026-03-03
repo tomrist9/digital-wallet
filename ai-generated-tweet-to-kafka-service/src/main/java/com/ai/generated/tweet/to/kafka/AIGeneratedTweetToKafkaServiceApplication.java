16cfb9cb22184121384da333bdd316301c447129
tomrist9 <tomristt9@gmail.com>
2026-03-03 15:10:49 +0400

    feat: add main and test classes

---
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/AIGeneratedTweetToKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.ai.generated.tweet.to.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AIGeneratedTweetToKafkaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AIGeneratedTweetToKafkaServiceApplication.class, args);
+    }
+}
---
+++ ai-generated-tweet-to-kafka-service/src/test/java/com/ai/generated/tweet/to/kafka/AIGeneratedTweetToKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai.generated.tweet.to.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIGeneratedTweetToKafkaServiceApplication {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
