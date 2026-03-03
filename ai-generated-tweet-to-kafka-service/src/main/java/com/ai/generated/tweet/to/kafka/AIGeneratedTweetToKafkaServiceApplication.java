34351096572356d4273e531a0ff393d9a17763ec
tomrist9 <tomristt9@gmail.com>
2026-03-03 15:48:29 +0400

    feat: add run method to the main class

--- ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/AIGeneratedTweetToKafkaServiceApplication.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/AIGeneratedTweetToKafkaServiceApplication.java
@@ -1,17 +1,27 @@
 package com.ai.generated.tweet.to.kafka;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AIGeneratedTweetToKafkaServiceApplication {
+public class AIGeneratedTweetToKafkaServiceApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(AIGeneratedTweetToKafkaServiceApplication.class, args);
     }
 
-    @PostConstruct
-    public void init(){
-
+    @Override
+    public void run(String... args) throws Exception {
+//        log.info("Application is starting...");
+//        boolean initResult = streamInitializer.init();
+//        if (initResult) {
+//            log.info("Starting AI Stream Runner with fixed rate {} seconds!", configData.getSchedulerDurationSec());
+//            taskScheduler.scheduleAtFixedRate(aiStreamRunner,
+//                    Duration.of(configData.getSchedulerDurationSec(), ChronoUnit.SECONDS));
+//        } else {
+//            log.error("Stream Initializer failed to initialize the streams! Not starting the AI Stream Runner!");
+//        }
     }
+
 }
