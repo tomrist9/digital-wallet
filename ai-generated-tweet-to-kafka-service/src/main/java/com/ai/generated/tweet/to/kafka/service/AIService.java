54042ed506e9e5e9d08b16dd5ae7cdaaaee39fe8
tomrist9 <tomristt9@gmail.com>
2026-03-15 14:55:00 +0400

    feat(ai): implement OpenAI Java client for tweet generation

--- ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/service/AIService.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/service/AIService.java
@@ -1,6 +1,8 @@
 package com.ai.generated.tweet.to.kafka.service;
 
 import com.ai.generated.tweet.to.kafka.exception.AIGeneratedTweetToKafkaServiceException;
+import org.springframework.stereotype.Component;
+
 
 public interface AIService {
     String generateTweet() throws AIGeneratedTweetToKafkaServiceException;
--- ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/service/OpenAIJavaClient.java
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/ai/generated/tweet/to/kafka/service/OpenAIJavaClient.java
@@ -3,12 +3,20 @@ package com.ai.generated.tweet.to.kafka.service;
 import com.ai.generated.tweet.to.kafka.config.AIGeneratedTweetToKafkaServiceConfigData;
 import com.ai.generated.tweet.to.kafka.exception.AIGeneratedTweetToKafkaServiceException;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.ChatModel;
+import com.openai.models.chat.completions.ChatCompletion;
+import com.openai.models.chat.completions.ChatCompletionCreateParams;
+import com.openai.models.chat.completions.ChatCompletionMessage;
 import lombok.RequiredArgsConstructor;
 import com.openai.client.OpenAIClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @ConditionalOnProperty(value = "ai-tweet-generator.ai-service", havingValue = "OpenAI-JavaClient")
 @RequiredArgsConstructor
+@Service
 public class OpenAIJavaClient implements AIService {
     private final AIGeneratedTweetToKafkaServiceConfigData configData;
     @Override
@@ -20,5 +28,22 @@ public class OpenAIJavaClient implements AIService {
 
     private String getPromptResponse(String prompt) {
         OpenAIClient client = OpenAIOkHttpClient.fromEnv();
+
+        ChatCompletionCreateParams.Builder createParams = ChatCompletionCreateParams.builder()
+                .model(ChatModel.of(configData.openAI().getModel()))
+                .addDeveloperMessage("Generate a tweet based on the specified format and the given keywords.")
+                .maxCompletionTokens(configData.openAI().getMaxCompletionTokens())
+                .temperature(configData.openAI().getTemperature())
+                .addUserMessage(prompt);
+
+        List<ChatCompletionMessage> messages = client.chat()
+                .completions()
+                .create(createParams.build())
+                .choices()
+                .stream()
+                .map(ChatCompletion.Choice::message)
+                .toList();
+
+        return messages.get(0).content().get();
     }
 }
